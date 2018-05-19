d9552ba4454bc0a12d6721bb99e78c882ebf6b27
BarWy <b.wysocki@onet.pl>
2018-05-19 10:48:03 +0200

    najnowsze

---
+++ js.js
@@ -0,0 +1,12 @@
+
+var $arrow =$("arrow a");
+
+var $off =  $("main").offset().top;
+
+$("#arrow").on("click", function(){
+    $("body, html").animate({
+        scrollTop: $("main").offset().top
+    }, 1000)
+
+})
+
